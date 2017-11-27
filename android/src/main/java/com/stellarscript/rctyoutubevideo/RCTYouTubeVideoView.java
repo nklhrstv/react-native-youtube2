489299f096dc88746e21846fe64d5f8fab5feca5
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2017-11-27 18:38:54 +0200

    YouTubeEventListener implemented in a separate file

--- android/src/main/java/com/stellarscript/rctyoutubevideo/RCTYouTubeVideoView.java
+++ android/src/main/java/com/stellarscript/rctyoutubevideo/RCTYouTubeVideoView.java
@@ -3,75 +3,18 @@ package com.stellarscript.rctyoutubevideo;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.events.RCTEventEmitter;
-import com.stellarscript.youtubevideo.YouTubeEventListener;
 import com.stellarscript.youtubevideo.YouTubeVideoView;
 
 final class RCTYouTubeVideoView extends FrameLayout {
 
     private final YouTubeVideoView mVideoView;
-    private final RCTEventEmitter mEventEmitter;
-    private final YouTubeEventListener mEventListener = new YouTubeEventListener() {
-
-        @Override
-        public void onError(final String message) {
-            final WritableMap event = Arguments.createMap();
-            event.putString(RCTYoutubeVideoEvents.ON_ERROR_MESSAGE_PROP, message);
-            event.putBoolean(RCTYoutubeVideoEvents.ON_ERROR_IS_CRITICAL_PROP, true);
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_ERROR_EVENT, event);
-        }
-
-        @Override
-        public void onTimeChanged(final int time) {
-            final WritableMap event = Arguments.createMap();
-            event.putInt(RCTYoutubeVideoEvents.ON_TIME_CHANGED_TIME_PROP, time);
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_TIME_CHANGED_EVENT, event);
-        }
-
-        @Override
-        public void onEndReached() {
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_END_REACHED_EVENT, null);
-        }
-
-        @Override
-        public void onPlaying(final int duration) {
-            final WritableMap event = Arguments.createMap();
-            event.putInt(RCTYoutubeVideoEvents.ON_PLAYING_DURATION_PROP, duration);
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_PLAYING_EVENT, event);
-        }
-
-        @Override
-        public void onPaused() {
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_PAUSED_EVENT, null);
-        }
-
-        @Override
-        public void onSeekPerformed() {
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_SEEK_PERFORMED_EVENT, null);
-        }
-
-        @Override
-        public void onBuffering(final int buffering) {
-            final WritableMap event = Arguments.createMap();
-            event.putInt(RCTYoutubeVideoEvents.ON_BUFFERING_BUFFERING_PROP, buffering);
-            mEventEmitter.receiveEvent(RCTYouTubeVideoView.this.getId(), RCTYoutubeVideoEvents.ON_BUFFERING_EVENT, event);
-        }
-
-        @Override
-        public void onReady() {
-        }
-
-    };
 
     public RCTYouTubeVideoView(final ThemedReactContext themedReactContext) {
         super(themedReactContext);
 
-        mEventEmitter = themedReactContext.getJSModule(RCTEventEmitter.class);
-
-        mVideoView = new YouTubeVideoView(themedReactContext, mEventListener);
+        final RCTYoutubeVideoEventEmitter eventEmitter = new RCTYoutubeVideoEventEmitter(RCTYouTubeVideoView.this, themedReactContext);
+        mVideoView = new YouTubeVideoView(themedReactContext, eventEmitter);
         final ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         addView(mVideoView, layoutParams);
     }
---
+++ android/src/main/java/com/stellarscript/rctyoutubevideo/RCTYoutubeVideoEventEmitter.java
@@ -0,0 +1,67 @@
+package com.stellarscript.rctyoutubevideo;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+import com.stellarscript.youtubevideo.YouTubeEventListener;
+
+final class RCTYoutubeVideoEventEmitter implements YouTubeEventListener {
+
+    private final RCTYouTubeVideoView mVideoView;
+    private final RCTEventEmitter mEventEmitter;
+
+    RCTYoutubeVideoEventEmitter(final RCTYouTubeVideoView videoView, final ThemedReactContext themedReactContext) {
+        mVideoView = videoView;
+        mEventEmitter = themedReactContext.getJSModule(RCTEventEmitter.class);
+    }
+
+    @Override
+    public void onError(final String message) {
+        final WritableMap event = Arguments.createMap();
+        event.putString(RCTYoutubeVideoEvents.ON_ERROR_MESSAGE_PROP, message);
+        event.putBoolean(RCTYoutubeVideoEvents.ON_ERROR_IS_CRITICAL_PROP, true);
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_ERROR_EVENT, event);
+    }
+
+    @Override
+    public void onTimeChanged(final int time) {
+        final WritableMap event = Arguments.createMap();
+        event.putInt(RCTYoutubeVideoEvents.ON_TIME_CHANGED_TIME_PROP, time);
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_TIME_CHANGED_EVENT, event);
+    }
+
+    @Override
+    public void onEndReached() {
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_END_REACHED_EVENT, null);
+    }
+
+    @Override
+    public void onPlaying(final int duration) {
+        final WritableMap event = Arguments.createMap();
+        event.putInt(RCTYoutubeVideoEvents.ON_PLAYING_DURATION_PROP, duration);
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_PLAYING_EVENT, event);
+    }
+
+    @Override
+    public void onPaused() {
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_PAUSED_EVENT, null);
+    }
+
+    @Override
+    public void onSeekPerformed() {
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_SEEK_PERFORMED_EVENT, null);
+    }
+
+    @Override
+    public void onBuffering(final int buffering) {
+        final WritableMap event = Arguments.createMap();
+        event.putInt(RCTYoutubeVideoEvents.ON_BUFFERING_BUFFERING_PROP, buffering);
+        mEventEmitter.receiveEvent(mVideoView.getId(), RCTYoutubeVideoEvents.ON_BUFFERING_EVENT, event);
+    }
+
+    @Override
+    public void onReady() {
+    }
+
+}
